b40159b25f9c42551da9292673cf28ebecbb5853
Caleb Brown <44173285+cgcbrown@users.noreply.github.com>
2024-12-28 17:59:18 +0000

    feat: improved movement

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
   def __init__(self, ai_game):
     """Initialize the ship and set its starting position"""
     self.screen = ai_game.screen
+    self.settings = ai_game.settings
     self.screen_rect = ai_game.screen.get_rect()
 
     self.image = pygame.image.load('images/ship.bmp')
@@ -13,15 +14,20 @@ class Ship:
 
     self.rect.midbottom = self.screen_rect.midbottom
 
+    # Store a flat for the ship's exact location for exact horizontal position.
+    self.x = float(self.rect.x)
+
     self.moving_right = False
     self.moving_left = False
   
   def update(self):
     """Update the ship's position based on the movement flag."""
-    if self.moving_right:
-      self.rect.x += 1
-    if self.moving_left:
-      self.rect.x -= 1
+    if self.moving_right and self.rect.right < self.screen_rect.right:
+      self.x += self.settings.ship_speed
+    if self.moving_left and self.rect.left > 0:
+      self.x -= self.settings.ship_speed
+    
+    self.rect.x = self.x
 
   def blitme(self):
     """Draw the ship at its current location"""
